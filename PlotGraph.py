0d33411c9413da948dca9e40b744fd61726f5d98
anshikak1 <143382493+anshikak1@users.noreply.github.com>
2023-09-21 08:29:25 +0000

    wrote code to publish graph

---
+++ PlotGraph.py
@@ -0,0 +1,23 @@
+from streamlit.logger import get_logger
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+
+
+LOGGER = get_logger(__name__)
+
+
+def run():
+  df = pd.read_csv('toy_dataset.csv')
+  city_counts = df['City'].value_counts()
+
+  city_counts.plot(kind='bar', color='skyblue')
+  plt.xlabel('City')
+  plt.ylabel('Count')
+  plt.title('Number of Individuals in Each City')
+  plt.xticks(rotation=45)  # Rotating x-axis labels for better readability
+  plt.tight_layout()
+  plt.show()
+
+if __name__ == "__main__":
+    run()
